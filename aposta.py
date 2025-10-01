26df19b84212825f8b37667e0ee306465be475be
ewerthonmaycon11 <ewerthonmaycon11@gmail.com>
2025-10-01 13:05:47 -0300

    Update aposta.py

--- aposta.py
+++ aposta.py
@@ -112,21 +112,23 @@ def init_db():
     );
     ''')
 
-        cur.execute("""
-    CREATE TABLE IF NOT EXISTS apostas (
-        id SERIAL PRIMARY KEY,
-        usuario_id INT NOT NULL REFERENCES usuarios(id) ON DELETE CASCADE,
-        valor NUMERIC(10,2) NOT NULL,
-        odd_total NUMERIC(10,2) NOT NULL,
-        retorno_potencial NUMERIC(10,2) NOT NULL,
-        selecoes JSONB NOT NULL,
-        data_hora TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-    """)
-    
-    conn.commit()
-    cur.close()
-    conn.close()
+
+    # criando a tabela, alinhamento correto
+cur.execute("""
+CREATE TABLE IF NOT EXISTS apostas (
+    id SERIAL PRIMARY KEY,
+    usuario_id INT NOT NULL REFERENCES usuarios(id) ON DELETE CASCADE,
+    valor NUMERIC(10,2) NOT NULL,
+    odd_total NUMERIC(10,2) NOT NULL,
+    retorno_potencial NUMERIC(10,2) NOT NULL,
+    selecoes JSONB NOT NULL,
+    data_hora TIMESTAMP NOT NULL DEFAULT NOW()
+);
+""")
+conn.commit()
+cur.close()
+
+        
 
     conn.commit()
 
@@ -751,6 +753,7 @@ def logout():
 # ------------------ RODAR ------------------
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
 
 
 
